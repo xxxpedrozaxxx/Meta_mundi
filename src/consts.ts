09009615a1047bd882d0b43d086f29376797ed8e
Davoalef <davoalef@gmail.com>
2024-10-09 15:18:15 -0500

    Header Update

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'MetaMundi';
+export const SITE_TITLE = 'Electiva IV';
 export const SITE_DESCRIPTION = '¡Bienvenidos!';
